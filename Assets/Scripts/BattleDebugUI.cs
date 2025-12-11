ad18281dacbab25b06687fe4c1db3111bf4ef819
Radioactive6363 <tomasburgos2002@outlook.com>
2025-12-11 19:40:09 -0300

    LastFix

--- Assets/Scripts/BattleDebugUI.cs
+++ Assets/Scripts/BattleDebugUI.cs
@@ -10,33 +10,70 @@ public class BattleDebugUI : MonoBehaviour
 
     private UnitBrain _redLeader;
     private UnitBrain _blueLeader;
+    
+    private float _searchTimer = 0f;
+    private float _searchInterval = 0.5f;
 
     void Start()
     {
         FindAndSubscribeLeaders();
     }
 
+    void Update()
+    {
+        _searchTimer += Time.deltaTime;
+        if (_searchTimer >= _searchInterval)
+        {
+            _searchTimer = 0f;
+            CheckLeadersStatus();
+        }
+    }
+
+    void CheckLeadersStatus()
+    {
+        if (_redLeader == null)
+        {
+            FindLeaderForTeam(BattleTeams.Red);
+        }
+        
+        if (_blueLeader == null)
+        {
+            FindLeaderForTeam(BattleTeams.Blue);
+        }
+    }
+
     void FindAndSubscribeLeaders()
     {
+        FindLeaderForTeam(BattleTeams.Red);
+        FindLeaderForTeam(BattleTeams.Blue);
+    }
+
+    void FindLeaderForTeam(BattleTeams team)
+    {
         var allUnits = FindObjectsByType<UnitBrain>(FindObjectsSortMode.None);
+        
         foreach (var unit in allUnits)
         {
-            if (unit.isLeader)
+            if (unit.teamID == team && unit.isLeader && unit.currentHealth > 0)
             {
-                if (unit.teamID == BattleTeams.Red)
+                if (team == BattleTeams.Red)
                 {
                     _redLeader = unit;
-                    _redLeader.OnStateChanged += UpdateRedText; 
-                    UpdateRedText(_redLeader.CurrentStateName);
+                    _redLeader.OnStateChanged += UpdateRedText;
+                    UpdateRedText(_redLeader.CurrentStateName); 
                 }
-                else if (unit.teamID == BattleTeams.Blue)
+                else if (team == BattleTeams.Blue)
                 {
                     _blueLeader = unit;
                     _blueLeader.OnStateChanged += UpdateBlueText;
-                    UpdateBlueText(_blueLeader.CurrentStateName);
+                    UpdateBlueText(_blueLeader.CurrentStateName); 
                 }
+                return; 
             }
         }
+        
+        if (team == BattleTeams.Red) redLeaderText.text = "RED LEADER:\nSearching...";
+        if (team == BattleTeams.Blue) blueLeaderText.text = "BLUE LEADER:\nSearching...";
     }
     
     void UpdateRedText(string stateName)
